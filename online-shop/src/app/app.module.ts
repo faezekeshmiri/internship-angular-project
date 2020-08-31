2612eb8589b411fbd983899d18dab036ef89e037
faeze <keshmirifaezeh@gmail.com>
2020-08-31 23:09:11 +0430

    Complete header. Closes #1

--- online-shop/src/app/app.module.ts
+++ online-shop/src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { FormsModule } from '@angular/forms';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [	
+    AppComponent,
+      HeaderComponent
+   ],
   imports: [
     BrowserModule,
     FormsModule
