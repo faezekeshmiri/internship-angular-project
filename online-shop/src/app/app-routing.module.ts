930561d5f2b8d9719c7abc88f2a3b58a0f5f65cb
faeze <keshmirifaezeh@gmail.com>
2020-09-01 15:02:58 +0430

    Add in app naigation. Closes #1

---
+++ online-shop/src/app/app-routing.module.ts
@@ -0,0 +1,19 @@
+import { LoginComponent } from './login/login.component';
+import { HomeComponent } from './home/home.component';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { ProductsComponent } from './products/products.component';
+
+
+const routes: Routes = [
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: 'products', component: ProductsComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'home', component: HomeComponent }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
--- online-shop/src/app/app.module.ts
+++ online-shop/src/app/app.module.ts
@@ -5,16 +5,26 @@ import { AppComponent } from './app.component';
 import { FormsModule } from '@angular/forms';
 import { HeaderComponent } from './header/header.component';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { CategoryComponent } from './Category/Category.component';
+import { ProductsComponent } from './products/products.component';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
-  declarations: [	
+  declarations: [					
     AppComponent,
-      HeaderComponent
+      HeaderComponent,
+      CategoryComponent,
+      ProductsComponent,
+      HomeComponent,
+      LoginComponent
    ],
   imports: [
     BrowserModule,
     FormsModule,
-    FontAwesomeModule
+    FontAwesomeModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
