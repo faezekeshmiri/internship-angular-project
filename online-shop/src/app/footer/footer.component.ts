fa633788bf909c566f4beb41e62c735aed88536d
faeze <keshmirifaezeh@gmail.com>
2020-09-02 20:02:42 +0430

    Complete footer component. Closes #6

--- online-shop/src/app/footer/footer.component.ts
+++ online-shop/src/app/footer/footer.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { faShoppingCart, faSearch , faBars , faSignInAlt} from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-footer',
@@ -6,7 +7,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./footer.component.css']
 })
 export class FooterComponent implements OnInit {
-
+  faShoppingCart = faShoppingCart;
+  faSearch = faSearch;
+  faBars = faBars;
+  faSignInAlt = faSignInAlt;
   constructor() { }
 
   ngOnInit() {
