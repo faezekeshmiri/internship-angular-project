940a0a124939f6fc2dac159516d0160d25795df0
faeze <keshmirifaezeh@gmail.com>
2020-09-06 22:43:11 +0430

    Add event binding to register button. Closes #10

--- online-shop/src/app/register/register.component.ts
+++ online-shop/src/app/register/register.component.ts
@@ -7,20 +7,26 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./register.component.css']
 })
 export class RegisterComponent implements OnInit {
-  users: User[] = [{id:0 , firstName:'faeze' , lastName:'kesh', password:'1234' , phoneNumber:'1234567'}];
+  users: User[] = [] ;
+  errorMessage: string;
   constructor() { }
 
   ngOnInit() {
+    this.errorMessage = '';
   }
-  addUser(firstName: string , lastName: string, phonNumber: string, password: string):void{
+  addUser(firstName: string , lastName: string, phonNumber: string, password: string): void{
+    this.errorMessage = '';
     const user: User = { id: -1 , firstName: '' , lastName: '' , phoneNumber: '' , password: ''};
     user.firstName = firstName;
     user.lastName = lastName;
     user.phoneNumber = phonNumber;
     user.password = password;
     user.id = this.users.length;
-    if (user){
+    if (user.firstName !== '' && user.lastName !== '' && user.phoneNumber !== '' && user.password !== ''){
       this.users.push(user);
+    }
+    else{
+      this.errorMessage = '!لطفا همه فیلدها را پر کنید';
     }
 
   }
