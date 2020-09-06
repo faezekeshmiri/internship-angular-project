a8f09ca67158b1c76cc05b25b368ba8d4e823dd3
faeze <keshmirifaezeh@gmail.com>
2020-09-06 20:37:02 +0430

    Complete register component. Closes #10

--- online-shop/src/app/register/register.component.ts
+++ online-shop/src/app/register/register.component.ts
@@ -1,3 +1,4 @@
+import { User } from './../user';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -6,10 +7,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./register.component.css']
 })
 export class RegisterComponent implements OnInit {
-
+  users: User[] = [{id:0 , firstName:'faeze' , lastName:'kesh', password:'1234' , phoneNumber:'1234567'}];
   constructor() { }
 
   ngOnInit() {
+  }
+  addUser(firstName: string , lastName: string, phonNumber: string, password: string):void{
+    const user: User = { id: -1 , firstName: '' , lastName: '' , phoneNumber: '' , password: ''};
+    user.firstName = firstName;
+    user.lastName = lastName;
+    user.phoneNumber = phonNumber;
+    user.password = password;
+    user.id = this.users.length;
+    if (user){
+      this.users.push(user);
+    }
+
   }
 
 }
